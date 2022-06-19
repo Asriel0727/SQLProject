5ee0d76abd683d24e63ace9b90459d81ca7d90ac
Asriel0727 <molly27molly@gmail.com>
2022-06-19 16:29:25 +0800

    SQLProject

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Project.Models;
+using PagedList;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbSCEntities db = new dbSCEntities();
+        // int pageSize = 2;
+
+        // GET: Home
+        public ActionResult Index()/*(int page=1)*/
+        {
+            //   int currentPage = page < 1 ? 1 : page;
+            var products = db.Product
+              .OrderByDescending(m => m.fId).ToList();
+            //    var result = products.ToPagedList(currentPage, pageSize);
+            return View(products);
+        }
+
+        //Get: Home/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //Post: Home/Login
+        [HttpPost]
+        public ActionResult Login(string mId, string mPwd)
+        {
+            // 依帳密取得會員並指定給member
+            var member = db.Member
+                .Where(m => m.mId == mId && m.mPwd == mPwd)
+                .FirstOrDefault();
+            //若member為null，表示會員未註冊
+            if (member == null)
+            {
+                ViewBag.Message = "帳密錯誤，登入失敗";
+                return View();
+            }
+            //使用Session變數記錄歡迎詞
+            Session["WelCome"] = member.mName + "歡迎光臨";
+            FormsAuthentication.RedirectFromLoginPage(mId, true);
+            return RedirectToAction("Index", "Member");
+        }
+
+        //Get:Home/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+        //Post:Home/Register
+        [HttpPost]
+        public ActionResult Register(Member pMember)
+        {
+            //若模型沒有通過驗證則顯示目前的View
+            if (ModelState.IsValid == false)
+            {
+                return View();
+            }
+            // 依帳號取得會員並指定給member
+            var member = db.Member
+                .Where(m => m.mId == pMember.mId)
+                .FirstOrDefault();
+            //若member為null，表示會員未註冊
+            if (member == null)
+            {
+                //將會員記錄新增到tMember資料表
+                db.Member.Add(pMember);
+                db.SaveChanges();
+                //執行Home控制器的Login動作方法
+                return RedirectToAction("Login");
+            }
+            ViewBag.Message = "此帳號己有人使用，註冊失敗";
+            return View();
+        }
+
+    }
+}
---
+++ Project/Controllers/MemberController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Project.Models;
+using System.Data;
+using System.Data.SqlClient;
+using PagedList;
+
+namespace Project.Controllers
+{
+
+    [Authorize]
+    public class MemberController : Controller
+    {
+
+        dbSCEntities db = new dbSCEntities();
+        int pageSize = 3;
+
+        public ActionResult Index()
+        {
+
+            var products = db.Product.OrderByDescending(m => m.fId).ToList();
+
+            return View("../Home/Index", "_LayoutMember", products);
+        }
+
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+
+
+        public ActionResult ShoppingCar()
+        {
+
+            string mId = User.Identity.Name;
+
+            var orderdetail = db.OrderDetail.Where
+                (m => m.mId == mId && m.oIsApproved == "否")
+                .ToList();
+
+            return View(orderdetail);
+        }
+
+
+
+        public ActionResult AddCar(string pId)
+        {
+
+            string mId = User.Identity.Name;
+
+            var currentCar = db.OrderDetail
+                .Where(m => m.pId == pId && m.oIsApproved == "否" && m.mId == mId)
+                .FirstOrDefault();
+
+            if (currentCar == null)
+            {
+
+                var product = db.Product.Where(m => m.pId == pId).FirstOrDefault();
+
+                OrderDetail orderdetail = new OrderDetail();
+                orderdetail.mId = mId;
+                orderdetail.pId = product.pId;
+                orderdetail.pName = product.pName;
+                orderdetail.pPrice = product.pPrice;
+                orderdetail.oNum = 1;
+                orderdetail.oIsApproved = "否";
+                db.OrderDetail.Add(orderdetail);
+            }
+            else
+            {
+
+                currentCar.oNum += 1;
+            }
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+
+
+        }
+
+
+
+
+        //Get:Member/DeleteCar
+        public ActionResult DeleteCar(int fId)
+        {
+
+            var orderDetail = db.OrderDetail.Where
+                (m => m.fId == fId).FirstOrDefault();
+
+            db.OrderDetail.Remove(orderDetail);
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+        }
+
+
+        [HttpPost]
+        public ActionResult ShoppingCar(string mName, string mEmail, string oAddress)
+        {
+            string mId = User.Identity.Name;
+            string oId = Guid.NewGuid().ToString();
+
+            Order order = new Order();
+            order.oId = oId;
+            order.mId = mId;
+            order.mName = mName;
+            order.mEmail = mEmail;
+            order.oAddress = oAddress;
+            order.oDate = DateTime.Now;
+            db.Order.Add(order);
+            var carList = db.OrderDetail
+                .Where(m => m.oIsApproved == "否" && m.mId == mId)
+                .ToList();
+
+            foreach (var item in carList)
+            {
+                item.oId = oId;
+                item.oIsApproved = "是";
+            }
+
+            db.SaveChanges();
+
+
+
+
+            return RedirectToAction("OrderList");
+
+
+        }
+
+
+        public ActionResult OrderList(int page = 1)
+        {
+            int currentPage = page < 1 ? 1 : page;
+            string mId = User.Identity.Name;
+            var orders = db.Order.Where(m => m.mId == mId)
+                .OrderByDescending(m => m.oDate).ToList();
+            var result = orders.ToPagedList(currentPage, pageSize);
+            return View(result);
+        }
+
+
+
+        public ActionResult OrderDetail(string oId)
+        {
+            var orderDetails = db.OrderDetail
+                .Where(m => m.oId == oId).ToList();
+            return View(orderDetails);
+        }
+        public ActionResult Edit(string oId)
+        {
+            var orderlist = db.Order
+               .Where(m => m.oId == oId).FirstOrDefault();
+            return View(orderlist);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Order oProduct)
+        {
+
+            // 修改資料
+            var orderlist = db.Order
+                .Where(m => m.oId == oProduct.oId).FirstOrDefault();
+            orderlist.mName = oProduct.mName;
+            orderlist.oAddress = oProduct.oAddress;
+            orderlist.mEmail = oProduct.mEmail;
+            db.SaveChanges();
+            return RedirectToAction("OrderList");
+        }
+
+    }
+
+}
